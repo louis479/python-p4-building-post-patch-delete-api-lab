ea9e6c4ac5d3a3ba9bbe84ab7d06bd5ee014d0bd
louis-ngugi <louisngugi858@gmail.com>
2025-06-26 02:01:45 +0300

    learnt build post-patch-delete api Lab

--- server/app.py
+++ server/app.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
+from flask import Flask, request, make_response, jsonify
+from flask_migrate import Migrate
 
+from models import db, Bakery, BakedGood
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
